03ad5ad8a8ec3a28d0033a39e0617a5835fdd5dd
Asif <asif.duet.cse19@gamil.com>
2024-10-19 22:52:47 +0600

    Added User theme and dashboard controller and AccountController

---
+++ MEAlumniAssociationDUET.Web/Controllers/AdminDashboard.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MEAlumniAssociationDUET.Web.Controllers
+{
+    public class AdminDashboard : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
